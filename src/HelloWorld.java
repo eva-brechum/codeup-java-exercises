a3dd091fd0fc733b8f7b47bbdbb5d4c8f7b17fd6
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-15 10:14:38 -0600

    finished java syntax types and variables exercise with walkthrough

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,53 +3,65 @@ public class HelloWorld {
 //      System.out.print("Hello ");
 //      System.out.print("World!");
 
+//        1.
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
 
-//      String myString = "Today was a good day!";
-//        System.out.print(myString);
+//        2.
+        String myString = "Today was a good day!";
+        System.out.print(myString);
 
+//      3.
 //      String myString = 'C';//incompatible types: char cannot be converted
 //      System.out.println(myString);
 
+//      4.
 //      String myString = 3.14159; //incompatible types: double cannot be converted
 //      System.out.println(myString );
 
+//      5.
 //      long myNumber;
 //      System.out.println(myNumber); //variable myNumber not initialized
 
+//      6.
 //      myNumber = 3.14; //incompatible: possible lossy conversion from double to long
 
+//      7.
 //      myNumber = 123L;
 //      System.out.println(myNumber);
-//
+
+//      8.
 //      myNumber = 123;
 //      System.out.println(myNumber);
 
+//      9.
 //      float myNumber = 3.14; //possible lossy conversion from double to float
 //      System.out.println(myNumber);
 //      9a.
-//       float myNumber = 3.14f;
+//      float myNumber = 3.14f;
 //      System.out.println(myNumber);
-//       b.
-        float myNumber = 3.14F;
+//      9b.
+        float myNumber = (float)3.14;
         System.out.println(myNumber);
 
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
+//      10.
+//      int x = 5;
+//      System.out.println(x++);
+//      System.out.println(x);
 //
 
 //        int x = 5;
 //        System.out.println(++x);
 //        System.out.println(x);
 
-//      class; //identifier expected
+//      11.
+//      String class = "hey"; //identifier expected
 //      System.out.println(class);
 
+//        12.
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
-//        int three = (int) o; incompatible
+//        int three = (int) o; //incompatible
 
 //        int three = (int) "three"; //incompatible types cannot be converted to int
 //        System.out.println(three);
@@ -64,7 +76,7 @@ public class HelloWorld {
 //        int x = 3;
 //        int y = 4;
 //        y = y * x;
-
+//
 //        int x = 3;
 //        int y = 4;
 //        y *= x;
