b9b19d7676f47e06035aec1888381b0c015b2d87
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-14 23:01:51 -0600

    completed twelve problems from the java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,16 +15,45 @@ public class HelloWorld {
 //      String myString = 3.14159; //incompatible types: double cannot be converted
 //      System.out.println(myString );
 
-        long myNumber;
-//        System.out.println(myNumber); //variable myNumber not initialized
+//      long myNumber;
+//      System.out.println(myNumber); //variable myNumber not initialized
 
-//        myNumber = 3.14; //incompatible: possible lossy conversion from double to long
+//      myNumber = 3.14; //incompatible: possible lossy conversion from double to long
 
-        myNumber = 123L;
+//      myNumber = 123L;
+//      System.out.println(myNumber);
+//
+//      myNumber = 123;
+//      System.out.println(myNumber);
+
+//      float myNumber = 3.14; //possible lossy conversion from double to float
+//      System.out.println(myNumber);
+//      9a.
+//       float myNumber = 3.14f;
+//      System.out.println(myNumber);
+//       b.
+        float myNumber = 3.14F;
         System.out.println(myNumber);
 
-        myNumber = 123;
-        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//      class; //identifier expected
+//      System.out.println(class);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o; incompatible
+
+//        int three = (int) "three"; //incompatible types cannot be converted to int
+//        System.out.println(three);
+
 
 
 
