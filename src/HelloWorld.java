6d68906a5fbe064a86b2ed1d1941bb6caeaa6358
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-14 22:02:02 -0600

    completed five problems from java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("World!");
+//      System.out.print("Hello ");
+//      System.out.print("World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+//      String myString = "Today was a good day!";
+//        System.out.print(myString);
+
+//      String myString = 'C';//incompatible types: char cannot be converted
+//      System.out.println(myString);
+
+//      String myString = 3.14159; //incompatible types: double cannot be converted
+//      System.out.println(myString );
+
+        long myNumber;
+//        System.out.println(myNumber); //variable myNumber not initialized
+
+//        myNumber = 3.14; //incompatible types: po
+
+
     }
 }
