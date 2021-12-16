77c32181cc41690d09b0354b932ceb81c812e34a
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-15 22:14:48 -0600

    completed java console exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,38 +4,51 @@ public class ConsoleExercises {
     public static void main(String[] args){
 //        #1
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %f", pi);
+//        System.out.format("The value of pi is approximately %f", pi);
+//        Or
+        System.out.printf("\n The value of pi is approximately %.2f. \n", pi);
 
 //        EXPLORING SCANNER CLASS
 //        #1
         Scanner scanner = new Scanner(System.in);
-//        System.out.println(" Please enter an integer");
-//        int a;
-//        a = scanner.nextInt();
-//        System.out.println("You have entered integer " + a);
+        System.out.println(" Please enter an integer");
+        int a;
+        a = scanner.nextInt();
+        System.out.println("You have entered integer " + a);
 //        if not an integer (Exception in thread main java.util)
 
 //        #2
 //        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 3 words");
-//        String text = "words";
-        String text1= scanner.next();
-        String text2 =scanner.next();
-        String text3= scanner.next();
-        System.out.println("You wrote "+ text1  + text2 + text3);
+//        System.out.println("Enter 3 words");
+////        String text = "words";
+//        String text1= scanner.next();
+//        String text2 =scanner.next();
+//        String text3= scanner.next();
+//        System.out.println("You wrote "+ text1  + text2 + text3);
 
 //        #3
-        System.out.println("Enter a sentence");
+//        System.out.println("Enter a sentence");
 //        String sentence = scanner.next();
 //        System.out.println(sentence);
 //        no it does not capture all the words because
 //         this method cannot read words with spaces in it
-        String sentence = scanner.nextLine();
-        System.out.println(sentence);
+//        String sentence = scanner.nextLine();
+//        System.out.println(sentence);
 
-//        Calculate the perimeter
+//        CALCULATE THE PERIMETER
 //        P= 2(l+w);
+//        #1
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter width of your room: ");
+//        int width = in.nextInt();
+//        int length = in.nextInt();
+//        System.out.println("Area is: " + width * length);
+//
+//        System.out.println("Enter the width and height: ");
+//        String input = scanner.nextLine();
 
+
+//        #2
         float length, width, area, perimeter;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the length of the rectangle:");
@@ -49,8 +62,8 @@ public class ConsoleExercises {
        area = length * width;
        System.out.println("Perimeter of rectangle " + perimeter + " units. ");
        System.out.println("Area of rectangle is " + area + " sq. units. ");
-//        System.out.println("The length of your room is " + length);
+        System.out.println("The length of your room is " + length);
 //        String width = scanner.nextLine();
-//        System.out.println("The width of your room is " + width);
+        System.out.println("The width of your room is " + width);
     }
 }
