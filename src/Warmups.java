db206d2f0419c4f66facef82f063891f767df024
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-16 09:19:30 -0600

    completed morning warmups

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Warmups {
     public static void main(String [] args){
-        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+//        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+
+
+        System.out.println("What did you eat for breakfast today?");
+        Scanner scanner = new Scanner(System.in);
+        String userBreakfast = scanner.nextLine();
+        System.out.printf("Your breakfast today is %s", userBreakfast);
     }
 }
