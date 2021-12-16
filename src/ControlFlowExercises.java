05c054917d54e52ad4c8e0e4f834adcd4973c413
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-16 14:36:46 -0600

    finished altering do while loop to count backwards by 5

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,9 +8,14 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 //        b. Do While
-       int i = 0;
-        do{
-            System.out.println(i +=2);
-        } while(i < 100);
+//       int i = 0;
+//        do{
+//            System.out.println(i +=2);
+//        } while(i < 100);
+
+        int i = 100;
+        do {
+            System.out.println(i -= 5);
+        }while(i >-10);
     }
 }
