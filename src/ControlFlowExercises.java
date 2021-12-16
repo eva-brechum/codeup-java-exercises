1875e9c512ab229e5b06b3718e9d0c0d704cc929
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-16 14:32:13 -0600

    working on the control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,16 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        LOOPS BASICS
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+//        a. While
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+//        b. Do While
+       int i = 0;
+        do{
+            System.out.println(i +=2);
+        } while(i < 100);
     }
 }
