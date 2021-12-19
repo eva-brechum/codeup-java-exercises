f2fc2a8cb06be7f8deadd8da792a6e36eaa6f633
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-18 21:37:32 -0600

    practice exercises for java

---
+++ src/JavaBasicsExercises.java
@@ -0,0 +1,20 @@
+public class JavaBasicsExercises {
+    public static void main(String[] args) {
+//        Write a Java program to print 'Hello' on screen and
+//        then print your name on a separate line.
+
+        System.out.print("Hello\nEva Brechum");
+
+//        Write a Java program to print the sum of two numbers.
+       System.out.println(2 + 2);
+
+//        Write a Java program to divide two numbers
+//        and print on the screen.
+        int x = 12;
+        int y = 2;
+
+        System.out.print(x/y);
+
+
+    }
+}
