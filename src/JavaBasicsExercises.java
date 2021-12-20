6caabf9b8b3190d78174407eb6fa6758ce0f3249
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-20 08:06:31 -0600

    java exercises

--- src/JavaBasicsExercises.java
+++ src/JavaBasicsExercises.java
@@ -3,17 +3,29 @@ public class JavaBasicsExercises {
 //        Write a Java program to print 'Hello' on screen and
 //        then print your name on a separate line.
 
-        System.out.print("Hello\nEva Brechum");
+//        System.out.print("Hello\nEva Brechum");
 
 //        Write a Java program to print the sum of two numbers.
-       System.out.println(2 + 2);
+//       System.out.println(2 + 2);
 
 //        Write a Java program to divide two numbers
 //        and print on the screen.
-        int x = 12;
-        int y = 2;
+//        int x = 50;
+//        int y = 3;
+//
+//        System.out.print(x/y);
 
-        System.out.print(x/y);
+//        Write a Java program to print the result of the following operations.
+//        a. -5 + 8 * 6
+//        b. (55+9) % 9
+//        c. 20 + -3*5 / 8
+//        d. 5 + 15 / 3 * 2 - 8 % 3
+
+        int x = -5;
+        int y = 8;
+        int z = 6;
+        System.out.print(x + y * z);
+
 
 
     }
