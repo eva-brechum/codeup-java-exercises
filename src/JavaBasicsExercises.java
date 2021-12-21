da30608f9b4cd9cdcf3d5538761585a8421ecefd
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-21 13:36:24 -0600

    practicing java problems

--- src/JavaBasicsExercises.java
+++ src/JavaBasicsExercises.java
@@ -20,13 +20,13 @@ public class JavaBasicsExercises {
 //        b. (55+9) % 9
 //        c. 20 + -3*5 / 8
 //        d. 5 + 15 / 3 * 2 - 8 % 3
-
-        int x = -5;
-        int y = 8;
-        int z = 6;
-        System.out.print(x + y * z);
-
-
-
+//
+//        int x = -5;
+//        int y = 8;
+//        int z = 6;
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) %9);
+        System.out.println(20 + -3*5/ 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
 }
