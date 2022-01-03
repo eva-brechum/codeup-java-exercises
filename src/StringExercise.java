f092301209db8eee76d617fcf760af4aa3b7dc2e
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-03 14:59:45 -0600

    finished walkthrough for strings basics

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 public class StringExercise {
     public static void main(String[] args) {
 //    Scanner sc = new Scanner(System.in);
-//        String output = "We don't need no education\n We don't need no thought control";
+//        String output = "We don\'t need no education\n We don't need no thought control";
 //        System.out.println(output);
 //    }
 
-//    String checkOut = "Check this out!, \"s inside of \"s!";
+//    String checkOut = "Check \"this out!, \"s inside of \"s!";
 //    System.out.println(checkOut);
 //    }
 
 //        String windows = "In windows, the main drive is usually C:\\";
 //        System.out.println(windows);
 //    }
-        String slash = "I can do backslashes\", double backslashes\\\", and the amazing triple backslash!";
-        System.out.println(slash);
-    }
+//        String slash = "I can do backslashes\", double backslashes\\\", and the amazing triple backslash!";
+//        System.out.println(slash);
+//    }
 }
