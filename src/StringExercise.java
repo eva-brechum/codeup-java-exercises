4c35d5c19c6e6afedcdd4ca78a328988004a6e10
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-03 14:10:24 -0600

    working on string exercise in Java

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StringExercise {
+    public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//        String output = "We don't need no education\n We dont need no thought control";
+//        System.out.println(output);
+//    }
+
+    String checkOut = "Check this out!, \"s inside of \"s!";
+    System.out.println(checkOut);
+    }
+
+//    String
+}
