324121ec3e863ff85d607818cb5fc8efc5e5d498
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-03 16:38:56 -0600

    working on methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,10 @@
+public class MethodsExercises {
+
+    public static int addition(int num1, int num2) {
+        return (num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addition(7,3));
+    }
+}
