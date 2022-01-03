f81e1c3363d071837309ef02c926d25b5755a2f9
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-03 16:48:46 -0600

    finished problem one in methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,7 +4,22 @@ public class MethodsExercises {
         return (num1 + num2);
     }
 
+    public static int subtract(int num1,int num2) {
+        return (num1 - num2);
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return (num1 * num2);
+    }
+
+    public static int division(int num1, int num2) {
+        return (num1 /num2);
+    }
+
     public static void main(String[] args) {
         System.out.println(addition(7,3));
+        System.out.println(subtract(8,4));
+        System.out.println(multiplication(5,5));
+        System.out.println(division(10,2));
     }
 }
