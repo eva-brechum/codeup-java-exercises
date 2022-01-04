075a8250937d1f98b6b0a89be2a92bd21596b442
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-03 23:52:40 -0600

    working on the bonus for the methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,6 @@
 public class MethodsExercises {
 
+//    1. BASIC ARITHMETIC
     public static int addition(int num1, int num2) {
         return (num1 + num2);
     }
@@ -16,10 +17,22 @@ public class MethodsExercises {
         return (num1 /num2);
     }
 
+    public static int remainder(int num1, int num2) {
+        return num1 % num2;
+    }
+
     public static void main(String[] args) {
         System.out.println(addition(7,3));
         System.out.println(subtract(8,4));
         System.out.println(multiplication(5,5));
         System.out.println(division(10,2));
+        System.out.println(remainder(3,2));
     }
+
+//    BONUS
+
+//    public static int userInt() {
+//
+//    };
+
 }
