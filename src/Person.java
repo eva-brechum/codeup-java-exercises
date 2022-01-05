5bb9bcf1eebff2bf83a49fff352dc5a8e3398b3a
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-05 11:30:58 -0600

    finished object basics

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,31 @@
 public class Person {
     private String name;
 
-    public Person(){
-    }
     public Person(String name) {
         this.name= name;
     }
 
+    public Person() {
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name =name;
+    }
+    public void sayHello() {
+        System.out.printf("\nHello, %s\n",this.name);
+    }
+
+    public static void main(String[] args) {
+        Person eva = new Person("eva");
+        eva.sayHello();
+        Person John = new Person();
+        John.setName("John");
+        System.out.println(John.getName());
+        John.sayHello();
+    }
 }
