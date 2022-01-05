acacbd30780bd8eba40fbc7902a0d689590e61fd
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-05 12:52:58 -0600

    working on input validation class

---
+++ src/util/Input.java
@@ -0,0 +1,14 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+    public String getString(){
+        return getString("Enter a string");
+    }
+
+}
