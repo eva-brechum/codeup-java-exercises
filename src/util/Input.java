6f8bacb5825392820599113fee4d8628f1e2c8e1
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-05 21:29:16 -0600

    working on boolean method in input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,17 +7,22 @@ import java.util.Scanner;
     public Input() {
         this.scanner = new Scanner(System.in);
     }
-//    public String getString() {
-//        return getString("Enter a string: ");
-
-        public String getString(){
+    public String getString(){
             String answer;
             System.out.println("Enter a string here: ");
             answer = this.scanner.next();
             return answer;
         }
+
+        public boolean yesNo(){
+            boolean answer;
+            System.out.print("\n Do you want to continue? (y/n): ");
+            answer = this.scanner.nextBoolean();
+            return answer;
+     }
     public static void main(String[] args) {
         Input input= new Input();
         System.out.println(input.getString());
+        System.out.println(input.yesNo());
     }
 }
