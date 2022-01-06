77f6021024fe6b30dccc9dcd2f34019dccf6a156
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-06 10:01:36 -0600

    working on morning warmups

---
+++ src/Penguin.java
@@ -0,0 +1,46 @@
+public class Penguin {
+    private String name;
+    private int weight;
+    private String species;
+    private String sex;
+
+    public Penguin(){
+
+    }
+
+    public Penguin(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getWeight(){
+    return weight;
+    }
+    public String getSpecies(){
+        return species;
+    }
+    public String getSex(){
+        return sex;
+    }
+    public void setName(){
+
+    }
+    public void setWeight(){
+
+    }
+    public void setSpecies(){
+
+    }
+    public void setSex(){
+
+    }
+
+    public static void canFly() {
+
+    }
+
+    public static void areEvil() {
+    }
+}
