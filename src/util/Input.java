72abbc5477d9850886c243f4a948e12dd40d648e
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-05 21:10:45 -0600

    completed the String method in input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,14 +1,23 @@
 package util;
 import java.util.Scanner;
 
-public class Input {
+ class Input {
     private Scanner scanner;
 
-    public Input(){
+    public Input() {
         this.scanner = new Scanner(System.in);
     }
-    public String getString(){
-        return getString("Enter a string");
-    }
+//    public String getString() {
+//        return getString("Enter a string: ");
 
+        public String getString(){
+            String answer;
+            System.out.println("Enter a string here: ");
+            answer = this.scanner.next();
+            return answer;
+        }
+    public static void main(String[] args) {
+        Input input= new Input();
+        System.out.println(input.getString());
+    }
 }
