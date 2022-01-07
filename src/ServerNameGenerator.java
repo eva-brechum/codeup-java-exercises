aa7ed09f66e072df5adb18104342c0a2cb0633f4
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-07 09:50:46 -0600

    working on server name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+        String[] adjectives = {"lovely", "brave", "short", "cool", "lucky", "perfect", "beautiful", "proud", "happy", "bored"};
+        String[] nouns = {"book", "dog", "heart", "phone", "truck", "car", "shoes", "cat", "laptop", "banana"};
+
+        System.out.println(adjectives.length);
+
+    }
+}
