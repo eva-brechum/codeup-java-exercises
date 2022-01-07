88a795159efdab5c29114a936b6ab20ed6116300
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-06 20:05:47 -0600

    working on array exercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ArraysExercises {
+    Person[] arr = new Person[3];
+
+
+    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);error we need Arrays.toString in order to get a string representation of the array;
+
+
+    }
+}
