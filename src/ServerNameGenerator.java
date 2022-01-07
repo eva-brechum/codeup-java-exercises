ecb148bee80fe9ac8f6212381200594a3324a335
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-07 10:30:30 -0600

    finished server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
+import java.util.Random;
 public class ServerNameGenerator {
-    public static void main(String[] args) {
 
+    public static String Random(String[] array) {
+        Random rand = new Random();
+        int index = rand.nextInt(array.length) + 1;
+        return array[index];
+    }
+
+    public static void main(String[] args) {
         String[] adjectives = {"lovely", "brave", "short", "cool", "lucky", "perfect", "beautiful", "proud", "happy", "bored"};
         String[] nouns = {"book", "dog", "heart", "phone", "truck", "car", "shoes", "cat", "laptop", "banana"};
 
-        System.out.println(adjectives.length);
-
+        System.out.printf("Here is your server name:%s-%s", Random(adjectives), Random(nouns));
     }
 }
