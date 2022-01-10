c64fc34e978b2ab4a5448616daa81a135c028cc4
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-09 22:45:41 -0600

    working on polymorphism and inheritance exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Rectangle {
+    protected int length;
+    protected int width;
+
+    public Rectangle (int lengthNum, int widthNum) {
+        this.length = lengthNum;
+        this.width= widthNum;
+    }
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimeter() {
+        return 2* length + 2 * width;
+    }
+}
