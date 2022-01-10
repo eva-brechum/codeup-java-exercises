0921c104225e325b870d2a9df0b44bad382ec195
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-10 12:35:05 -0600

    working on interfaces and abstract classes

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable{
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(){
+        this.length = length;
+        this.width = width;
+    }
+}
