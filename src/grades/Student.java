6aec1a47f7a7f34e6601110c25ce241aae5f51c4
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-11 12:34:11 -0600

    working on student grades app

---
+++ src/grades/Student.java
@@ -0,0 +1,21 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private String grades;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> StudentNames = new ArrayList<String>();
+
+
+    }
+    }
+}
