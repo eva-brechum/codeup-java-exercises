276237da5764417bead21cf3c7de9d2de74c21b4
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-11 14:27:47 -0600

    working on student grades app

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,15 +4,34 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    ArrayList<Integer> grades;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
     public String getName() {
         return name;
     }
     public void addGrade(int grade){
         this.grades.add(grade);
     }
-    public Student(ArrayList<Integer> grades) {
-        this.grades = grades;
+    public double getGradeAverage(){
+        double sum = 0;
+        for(Integer grade: this.grades){
+            sum += grade;
+        }
+        return sum/ this.grades.size();
+    }
+
+    public static void main(String[] args) {
+Student eva = new Student("Eva");
+    eva.addGrade(98);
+        eva.addGrade(75);
+        eva.addGrade(50);
+
+        System.out.println(eva.getGradeAverage());
     }
 }
 
