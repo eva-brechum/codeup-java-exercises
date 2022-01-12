46419b958457fd46cf9e015ab6c57ad26159977f
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-11 23:48:33 -0600

    working on student grades app

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -8,16 +8,32 @@ import java.util.HashMap;
 //and values that are Student objects. Create at least 4 Student objects
 //with at least 3 grades each, and add them to the map.
 public class GradesApplication {
+
     public static void main(String[] args) {
         // Inside the main method, create a HashMap named students.
-        HashMap<String,Student> students = new HashMap<>();
-//        Student Dan = new Student("Dan", 95);
-        Student s2 = new Student("Tom");
-        Student s3 = new Student("Jane");
-        Student s4 = new Student("Kat");
-
-//        students.put("Dan", s1);
+        HashMap<String,String> students = new HashMap<>();
+        students.put("EvaDiva", "Eva");
+        students.put("Tomclumsy", "Tom");
+        students.put("pourjane", "Jane");
+        students.put("hellKat", "Kat");
         System.out.println(students);
+
+        Student s1 = new Student("Eva");
+        s1.addGrade(95);
+        s1.addGrade(77);
+        s1.addGrade(80);
+        Student tom= new Student("Tom");
+        tom.addGrade(84);
+        tom.addGrade(69);
+        tom.addGrade(90);
+        Student s3 = new Student("Jane");
+        s3.addGrade(88);
+        s3.addGrade(94);
+        s3.addGrade(86);
+        Student s4 = new Student("Kat");
+    s4.addGrade(91);
+
+
 
     }
 
