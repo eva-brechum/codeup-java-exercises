236d4e7fa4fa53b05dbfaf91867763fa6d99fbec
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-20 22:30:41 -0600

    working on high low class

---
+++ src/HighLow.java
@@ -0,0 +1,23 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighLow {
+
+    public static void main(String[] args) {
+        int max =100;
+        int min = 1;
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int highLow = min + random.nextInt(max);
+        String userResponse;
+        System.out.println("Guess a random number between 1-100");
+        int userInput = Integer.parseInt(sc.nextLine());
+        if (userInput < ) {
+            System.out.println();
+        }
+
+
+
+    }
+}
