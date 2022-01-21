f9be79553c8eb08467ae46409f86a6a4d909227b
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-20 22:41:36 -0600

    updated high low class

--- src/HighLow.java
+++ src/HighLow.java
@@ -9,15 +9,18 @@ public class HighLow {
         int min = 1;
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        int highLow = min + random.nextInt(max);
-        String userResponse;
+        int num = min + random.nextInt(max);
+//        String userInput;
         System.out.println("Guess a random number between 1-100");
-        int userInput = Integer.parseInt(sc.nextLine());
-        if (userInput < ) {
-            System.out.println();
+        int userInput = Integer.parseInt(sc.next());
+//        userInput = sc.next();
+        if (userInput < num) {
+            System.out.println("HIGHER!");
+        } else if(userInput > num) {
+            System.out.println("LOWER!");
+        } else {
+            System.out.println("GOOD GUESS");
         }
-
-
 
     }
 }
