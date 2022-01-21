47845af91c83361362dcdb2c95a037c26a5f86b8
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-20 21:37:33 -0600

    completed bob class exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,7 +1,21 @@
-//import java.util.Scanner;
-//public class Bob {
-//    public static void main(String[] args) {
-//    Scanner sc= new Scanner(System.in);
-//
-//    }
-//}
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    String input = sc.nextLine();
+    if(input.endsWith("?")){
+        System.out.println("Sure.");
+    }else if (input.endsWith("!")){
+        System.out.println("Whoa, chill out!");
+    }else if(input.isEmpty()){
+        System.out.println("Fine. Be that way!");
+    }else{
+        System.out.println("Whatever.");
+    }
+        System.out.println("Do you want to continue talking? [y/N] ");
+    String answer = sc.nextLine();
+    if(answer.equals("N")) {
+        System.out.println("Ok! BYE THEN!");
+    }
+    }
+}
